6630dc000fdff9b538d854fc88142f2faa62ade4
Laurikainen <ingridlaurikainen@gmail.com>
2019-10-19 20:42:25 +0300

    New charecteristics for the map

--- js/map.js
+++ js/map.js
@@ -1,27 +1,33 @@
 window.onload = function () {
-	var mymap = L.map('mapid').setView([58.3,27.1], 11);
-	var mark = L.marker([58.3,27.1]).addTo(mymap);
-	var marks = L.layerGroup([mark]);
+	var mymap = L.map('mapid').setView([58.373, 26.716], 9);
+	var mark = L.marker([58.373, 26.716], {title: 'Vanemuise 46, Tartu'}).addTo(mymap);
 	
 	var base = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mymap);
 	
 	var layer1 = L.tileLayer.wms('https://aoraki.domenis.ut.ee/geoserver/spatialdatastudio2019/wms?', {
+	layers: 'spatialdatastudio2019:Tee',
+	format: 'image/png',
+	transparent: true
+	}).addTo(mymap);
+	
+	var layer2 = L.tileLayer.wms('https://aoraki.domenis.ut.ee/geoserver/spatialdatastudio2019/wms?', {
 	layers: 'spatialdatastudio2019:camping_sites_new-point',
 	format: 'image/png',
 	transparent: true
 	}).addTo(mymap);
 	
-	var layer1 = L.tileLayer.wms('https://aoraki.domenis.ut.ee/geoserver/topp/wms?', {
-	layers: 'topp:states',
+	var layer3 = L.tileLayer.wms('https://aoraki.domenis.ut.ee/geoserver/spatialdatastudio2019/wms?', {
+	layers: 'spatialdatastudio2019:hiking-line',
 	format: 'image/png',
 	transparent: true
 	}).addTo(mymap);
 	
 	var control = L.control.layers({}, {
-		'Camping sites': layer1,
-		'Layer 2': layer2
+		'Roads': layer1,
+		'Camping sites': layer2,
+		'Hiking trails': layer3		
 	})
 	control.addTo(mymap);
 };
